48e42669aaa8601415b13111b9f9e4fc669e143b
Слава Липин <Kompoman32@yandex.ru>
2018-12-06 13:57:25 +0500

    Add toString() to all classes

---
+++ WowCaseApp/Model/ModelExtention.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WowCaseApp.Model
+{
+    partial class Attribute
+    {
+        public override string ToString()
+        {
+            return $"{Table.Name}.{Name}";
+        }
+    }
+
+    partial class AttributeInForm
+    {
+        public override string ToString()
+        {
+            return Origin.ToString();
+        }
+    }
+
+    partial class AttributeInReport
+    {
+        public override string ToString()
+        {
+            return Origin.ToString();
+        }
+    }
+
+    partial class Form
+    {
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+
+    partial class Query
+    {
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+
+    partial class Report
+    {
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+
+    partial class Table
+    {
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
