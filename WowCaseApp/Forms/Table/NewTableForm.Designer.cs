c31b545a1eaf1769af4fd1bacc47e6ddb0655b26
Ilya Nurullin <nurullinin@mail.ru>
2018-12-08 17:10:39 +0500

    Добавил поле по умолчанию

--- WowCaseApp/Forms/Table/NewTableForm.Designer.cs
+++ WowCaseApp/Forms/Table/NewTableForm.Designer.cs
@@ -28,36 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.AttrType = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.IsPK = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.IsNullable = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.IsIndexed = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DefaultValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Title,
             this.AttrType,
             this.IsPK,
             this.IsNullable,
-            this.IsIndexed});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 28);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1128, 577);
-            this.dataGridView1.TabIndex = 0;
+            this.IsIndexed,
+            this.DefaultValue});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 28);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1128, 577);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1128, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            this.создатьToolStripMenuItem.Click += new System.EventHandler(this.создатьToolStripMenuItem_Click);
+            // 
+            // добавитьСсылкуНаДругуюТаблицыToolStripMenuItem
+            // 
+            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem.Name = "добавитьСсылкуНаДругуюТаблицыToolStripMenuItem";
+            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem.Size = new System.Drawing.Size(273, 24);
+            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem.Text = "Добавить ссылку на другую таблицу";
             // 
             // Title
             // 
@@ -81,55 +109,45 @@
             "Дата и время",
             "Да/нет"});
             this.AttrType.Name = "AttrType";
-            this.AttrType.Width = 200;
+            this.AttrType.Width = 150;
             // 
             // IsPK
             // 
             this.IsPK.HeaderText = "Является ПК?";
             this.IsPK.Name = "IsPK";
-            this.IsPK.Width = 120;
+            this.IsPK.Width = 110;
             // 
             // IsNullable
             // 
             this.IsNullable.HeaderText = "Может быть пустым?";
             this.IsNullable.Name = "IsNullable";
-            this.IsNullable.Width = 170;
+            this.IsNullable.Width = 160;
             // 
             // IsIndexed
             // 
             this.IsIndexed.HeaderText = "Индексируется?";
             this.IsIndexed.Name = "IsIndexed";
-            this.IsIndexed.Width = 150;
+            this.IsIndexed.Width = 130;
             // 
-            // menuStrip1
+            // DefaultValue
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1128, 28);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // добавитьСсылкуНаДругуюТаблицыToolStripMenuItem
-            // 
-            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem.Name = "добавитьСсылкуНаДругуюТаблицыToolStripMenuItem";
-            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem.Size = new System.Drawing.Size(273, 24);
-            this.добавитьСсылкуНаДругуюТаблицыToolStripMenuItem.Text = "Добавить ссылку на другую таблицу";
+            this.DefaultValue.HeaderText = "По умолчанию";
+            this.DefaultValue.Name = "DefaultValue";
+            this.DefaultValue.Width = 150;
             // 
             // NewTableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1128, 605);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "NewTableForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Создание новой таблицы";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -139,13 +157,15 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem добавитьСсылкуНаДругуюТаблицыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
         private System.Windows.Forms.DataGridViewTextBoxColumn Title;
         private System.Windows.Forms.DataGridViewComboBoxColumn AttrType;
         private System.Windows.Forms.DataGridViewCheckBoxColumn IsPK;
         private System.Windows.Forms.DataGridViewCheckBoxColumn IsNullable;
         private System.Windows.Forms.DataGridViewCheckBoxColumn IsIndexed;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem добавитьСсылкуНаДругуюТаблицыToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DefaultValue;
     }
 }
--- WowCaseApp/Forms/Table/NewTableForm.cs
+++ WowCaseApp/Forms/Table/NewTableForm.cs
@@ -20,5 +20,27 @@ namespace WowCaseApp
             InitializeComponent();
             log.Debug("NewTableForm opened");
         }
+
+        private void создатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var indexOf = new Dictionary<String, int>()
+            {
+                { "title", 0 },
+                { "type", 1},
+                { "isPK", 2},
+                { "isNullable", 3},
+                { "isIndexed", 4},
+            };
+            DataGridViewRowCollection attributes = dataGridView.Rows;
+
+            foreach (DataGridViewRow attribute in attributes)
+            {
+                log.Debug(attribute.Cells[indexOf["title"]].EditedFormattedValue.ToString());
+                log.Debug(attribute.Cells[indexOf["type"]].EditedFormattedValue.ToString());
+                log.Debug(attribute.Cells[indexOf["isPK"]].EditedFormattedValue.ToString());
+                log.Debug(attribute.Cells[indexOf["isNullable"]].EditedFormattedValue.ToString());
+                log.Debug(attribute.Cells[indexOf["isIndexed"]].EditedFormattedValue.ToString());
+            }
+        }
     }
 }
