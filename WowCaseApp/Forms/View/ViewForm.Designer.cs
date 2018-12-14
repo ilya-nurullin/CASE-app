f3cb8614bc6d3edc530562ffb53480d17123eeb2
Слава Липин <Kompoman32@yandex.ru>
2018-12-14 20:17:40 +0500

    add LoadForm

--- WowCaseApp/Forms/View/ViewForm.Designer.cs
+++ WowCaseApp/Forms/View/ViewForm.Designer.cs
@@ -34,8 +34,8 @@
             this.countLabel = new System.Windows.Forms.Label();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonNextVal = new System.Windows.Forms.Button();
+            this.buttonPrevVal = new System.Windows.Forms.Button();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.downButton = new System.Windows.Forms.Button();
             this.upButton = new System.Windows.Forms.Button();
@@ -89,8 +89,8 @@
             this.panel1.Controls.Add(this.countLabel);
             this.panel1.Controls.Add(this.radioButton2);
             this.panel1.Controls.Add(this.radioButton1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.buttonNextVal);
+            this.panel1.Controls.Add(this.buttonPrevVal);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(3, 2);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -112,7 +112,7 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(511, 12);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(103, 21);
             this.radioButton2.TabIndex = 3;
@@ -124,7 +124,7 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
             this.radioButton1.Location = new System.Drawing.Point(389, 12);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(94, 21);
             this.radioButton1.TabIndex = 2;
@@ -132,25 +132,27 @@
             this.radioButton1.Text = "Просмотр";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // buttonNextVal
             // 
-            this.button2.Location = new System.Drawing.Point(236, 5);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 39);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Следующий >";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonNextVal.Location = new System.Drawing.Point(236, 5);
+            this.buttonNextVal.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonNextVal.Name = "buttonNextVal";
+            this.buttonNextVal.Size = new System.Drawing.Size(133, 39);
+            this.buttonNextVal.TabIndex = 1;
+            this.buttonNextVal.Text = "Следующий >";
+            this.buttonNextVal.UseVisualStyleBackColor = true;
+            this.buttonNextVal.Click += new System.EventHandler(this.buttonNextVal_Click);
             // 
-            // button1
+            // buttonPrevVal
             // 
-            this.button1.Location = new System.Drawing.Point(9, 5);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 39);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "< Предыдущий";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonPrevVal.Location = new System.Drawing.Point(9, 5);
+            this.buttonPrevVal.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonPrevVal.Name = "buttonPrevVal";
+            this.buttonPrevVal.Size = new System.Drawing.Size(133, 39);
+            this.buttonPrevVal.TabIndex = 0;
+            this.buttonPrevVal.Text = "< Предыдущий";
+            this.buttonPrevVal.UseVisualStyleBackColor = true;
+            this.buttonPrevVal.Click += new System.EventHandler(this.buttonPrevVal_Click);
             // 
             // tabPage1
             // 
@@ -182,7 +184,7 @@
             // 
             this.downButton.Enabled = false;
             this.downButton.Location = new System.Drawing.Point(599, 169);
-            this.downButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.downButton.Margin = new System.Windows.Forms.Padding(4);
             this.downButton.Name = "downButton";
             this.downButton.Size = new System.Drawing.Size(32, 28);
             this.downButton.TabIndex = 13;
@@ -194,7 +196,7 @@
             // 
             this.upButton.Enabled = false;
             this.upButton.Location = new System.Drawing.Point(599, 127);
-            this.upButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.upButton.Margin = new System.Windows.Forms.Padding(4);
             this.upButton.Name = "upButton";
             this.upButton.Size = new System.Drawing.Size(32, 28);
             this.upButton.TabIndex = 12;
@@ -370,8 +372,8 @@
         private System.Windows.Forms.Label countLabel;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonNextVal;
+        private System.Windows.Forms.Button buttonPrevVal;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Button downButton;
         private System.Windows.Forms.Button upButton;
