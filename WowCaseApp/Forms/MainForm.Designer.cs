cab7d09f92869c0970560b34a659d48352844428
Слава Липин <Kompoman32@yandex.ru>
2018-12-15 14:27:12 +0500

    Вернул дизайнер на место

--- WowCaseApp/Forms/MainForm.Designer.cs
+++ WowCaseApp/Forms/MainForm.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Таблицы");
-            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Формы");
-            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Запросы");
-            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Отчеты");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Таблицы");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Формы");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Запросы");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Отчеты");
             this.MainTreeView = new System.Windows.Forms.TreeView();
             this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -58,26 +58,26 @@
             // MainTreeView
             // 
             this.MainTreeView.Dock = System.Windows.Forms.DockStyle.Left;
-            this.MainTreeView.Location = new System.Drawing.Point(0, 24);
-            this.MainTreeView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MainTreeView.Location = new System.Drawing.Point(0, 28);
+            this.MainTreeView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MainTreeView.Name = "MainTreeView";
-            treeNode17.Name = "Tables";
-            treeNode17.Tag = "Tables";
-            treeNode17.Text = "Таблицы";
-            treeNode18.Name = "Forms";
-            treeNode18.Tag = "Views";
-            treeNode18.Text = "Формы";
-            treeNode19.Name = "Queries";
-            treeNode19.Tag = "Queries";
-            treeNode19.Text = "Запросы";
-            treeNode20.Name = "Reports";
-            treeNode20.Tag = "Reports";
-            treeNode20.Text = "Отчеты";
+            treeNode5.Name = "Tables";
+            treeNode5.Tag = "Tables";
+            treeNode5.Text = "Таблицы";
+            treeNode6.Name = "Forms";
+            treeNode6.Tag = "Views";
+            treeNode6.Text = "Формы";
+            treeNode7.Name = "Queries";
+            treeNode7.Tag = "Queries";
+            treeNode7.Text = "Запросы";
+            treeNode8.Name = "Reports";
+            treeNode8.Tag = "Reports";
+            treeNode8.Text = "Отчеты";
             this.MainTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode17,
-            treeNode18,
-            treeNode19,
-            treeNode20});
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8});
             this.MainTreeView.Size = new System.Drawing.Size(110, 573);
             this.MainTreeView.TabIndex = 0;
             this.MainTreeView.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainTreeView_MouseUp);
@@ -89,8 +89,8 @@
             this.файлToolStripMenuItem});
             this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MainMenuStrip.Name = "MainMenuStrip";
-            this.MainMenuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.MainMenuStrip.Size = new System.Drawing.Size(1033, 24);
+            this.MainMenuStrip.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.MainMenuStrip.Size = new System.Drawing.Size(1377, 28);
             this.MainMenuStrip.TabIndex = 1;
             this.MainMenuStrip.Text = "menuStrip1";
             // 
@@ -99,13 +99,13 @@
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.создатьНовыйToolStripMenuItem});
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
             // создатьНовыйToolStripMenuItem
             // 
             this.создатьНовыйToolStripMenuItem.Name = "создатьНовыйToolStripMenuItem";
-            this.создатьНовыйToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.создатьНовыйToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
             this.создатьНовыйToolStripMenuItem.Text = "Создать новый";
             this.создатьНовыйToolStripMenuItem.Click += new System.EventHandler(this.создатьНовыйToolStripMenuItem_Click);
             // 
@@ -115,12 +115,12 @@
             this.tablesContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.создатьНовуюТаблицуToolStripMenuItem});
             this.tablesContextMenu.Name = "tablesContextMenu";
-            this.tablesContextMenu.Size = new System.Drawing.Size(205, 26);
+            this.tablesContextMenu.Size = new System.Drawing.Size(243, 28);
             // 
             // создатьНовуюТаблицуToolStripMenuItem
             // 
             this.создатьНовуюТаблицуToolStripMenuItem.Name = "создатьНовуюТаблицуToolStripMenuItem";
-            this.создатьНовуюТаблицуToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.создатьНовуюТаблицуToolStripMenuItem.Size = new System.Drawing.Size(242, 24);
             this.создатьНовуюТаблицуToolStripMenuItem.Text = "Создать новую таблицу";
             this.создатьНовуюТаблицуToolStripMenuItem.Click += new System.EventHandler(this.создатьНовуюТаблицуToolStripMenuItem_Click);
             // 
@@ -130,12 +130,12 @@
             this.queriesMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.создатьНовыйЗапросToolStripMenuItem1});
             this.queriesMenuStrip.Name = "queriesMenuStrip";
-            this.queriesMenuStrip.Size = new System.Drawing.Size(198, 26);
+            this.queriesMenuStrip.Size = new System.Drawing.Size(237, 28);
             // 
             // создатьНовыйЗапросToolStripMenuItem1
             // 
             this.создатьНовыйЗапросToolStripMenuItem1.Name = "создатьНовыйЗапросToolStripMenuItem1";
-            this.создатьНовыйЗапросToolStripMenuItem1.Size = new System.Drawing.Size(197, 22);
+            this.создатьНовыйЗапросToolStripMenuItem1.Size = new System.Drawing.Size(236, 24);
             this.создатьНовыйЗапросToolStripMenuItem1.Text = "Создать новый запрос";
             this.создатьНовыйЗапросToolStripMenuItem1.Click += new System.EventHandler(this.создатьНовыйЗапросToolStripMenuItem1_Click);
             // 
@@ -145,13 +145,13 @@
             this.viewsMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.создатьНовуюФормуToolStripMenuItem1});
             this.viewsMenuStrip.Name = "queriesMenuStrip";
-            this.viewsMenuStrip.Size = new System.Drawing.Size(198, 26);
+            this.viewsMenuStrip.Size = new System.Drawing.Size(233, 56);
             this.viewsMenuStrip.Tag = "Views";
             // 
             // создатьНовуюФормуToolStripMenuItem1
             // 
             this.создатьНовуюФормуToolStripMenuItem1.Name = "создатьНовуюФормуToolStripMenuItem1";
-            this.создатьНовуюФормуToolStripMenuItem1.Size = new System.Drawing.Size(197, 22);
+            this.создатьНовуюФормуToolStripMenuItem1.Size = new System.Drawing.Size(232, 24);
             this.создатьНовуюФормуToolStripMenuItem1.Text = "Создать новую форму";
             this.создатьНовуюФормуToolStripMenuItem1.Click += new System.EventHandler(this.создатьНовуюФормуToolStripMenuItem1_Click);
             // 
@@ -193,13 +193,13 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1033, 597);
+            this.ClientSize = new System.Drawing.Size(1377, 735);
             this.Controls.Add(this.MainTreeView);
             this.Controls.Add(this.MainMenuStrip);
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Wow Case App";
