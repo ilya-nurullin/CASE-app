2ad3352afcd8d049fb41500ed5fcfb0de594bc83
Слава Липин <Kompoman32@yandex.ru>
2018-12-16 22:37:00 +0500

    Добавил форму для добавления значения в таблицы

---
+++ WowCaseApp/Forms/View/NewValueForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WowCaseApp.Forms.View
+{
+    partial class NewValueForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(427, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(148, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите таблицу";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 68);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(427, 221);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // NewValueForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 289);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(443, 170);
+            this.Name = "NewValueForm";
+            this.Text = "Новое значение";
+            this.Load += new System.EventHandler(this.NewValueForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ WowCaseApp/Forms/View/NewValueForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+using WowCaseApp.Model;
+using Attribute = WowCaseApp.Model.Attribute;
+
+namespace WowCaseApp.Forms.View
+{
+    public partial class NewValueForm : Form
+    {
+        static readonly ILog _log = LogManager.GetLogger(typeof(ViewForm));
+        MetaDataDBContainer _container;
+        SqlConnection _dbConnection;
+
+        public NewValueForm(MetaDataDBContainer Container, SqlConnection dbConnection)
+        {
+            InitializeComponent();
+            _container = Container;
+            _dbConnection = dbConnection;
+        }
+
+        private void NewValueForm_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+
+            foreach (var t in _container.TableSet)
+                comboBox1.Items.Add(t);
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            Table t = comboBox1.SelectedItem as Table;
+
+            flowLayoutPanel1.Controls.Clear();
+            GenerateComponents(t?.Attributes);
+        }
+
+        void GenerateComponents(IEnumerable<Attribute> attributes)
+        {
+            foreach (var a in attributes)
+            {
+                if (a.IsPKey && a.Type == "Автоинкремент")
+                    continue;
+
+                Control c = new Control();
+                string type = a.Type;
+                if (a.IsFKey)
+                    type = _container.TableSet.First(x => x.RealName == a.Type).Attributes.First(x => x.IsPKey).Type;
+                switch (type)
+                {
+                    case "Автоинкремент":
+                    case "Целое число со знаком":
+                    case "Дробное число":
+                    c = new TextBox();
+                    break;
+
+                    case "Строка":
+                    case "Текст":
+                    c = new TextBox();
+                    break;
+
+                    case "Дата":
+                    case "Дата и время":
+                    c = new DateTimePicker();
+                    break;
+
+                    case "Да/нет":
+                    c = new CheckBox();
+                    ((CheckBox)c).CheckedChanged += (o, e) =>
+                    {
+                        ((CheckBox)o).Text = ((CheckBox)o).Checked.ToString();
+                    };
+                    break;
+                }
+
+                c.Name = $"{a.RealName}";
+
+                c.Margin = new Padding(50, 5, 0, 5);
+
+                Control label = new Label() { Text = a.Name, Margin = new Padding(50,5,0,5), AutoSize = true};
+                label.BackColor = Color.Transparent;
+
+                flowLayoutPanel1.Controls.Add(label);
+                flowLayoutPanel1.Controls.Add(c);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!(comboBox1.SelectedItem is Table t)) return;
+
+            var cols = "";
+            var values = "";
+
+            foreach (Control c in flowLayoutPanel1.Controls)
+            {
+                if (c is Label) continue;
+
+                cols += $"{c.Name}, ";
+
+                if (c is DateTimePicker dtp)
+                    values += $"'{dtp.Value.ToShortDateString()}', ";
+                else values += $"'{c.Text}', ";
+            }
+
+            cols = cols.Trim(',', ' ');
+            values = values.Trim(',', ' ');
+
+            var sql = $"INSERT INTO {t.RealName} ({cols}) VALUES ({values})";
+
+            try
+            {
+                SqlExecutor.ExecuteNonQuery(_dbConnection,sql);
+
+                MessageBox.Show($"Значение добавлено в таблицу '{t.Name}'");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Возникла ошибка при записи значения, возможно запрос некорректный","Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                _log.Error(ex);
+            }
+        }
+    }
+}
