054dc64fab473739d4556fe747d6be22e4384003
Ilya Nurullin <nurullinin@mail.ru>
2018-12-16 10:24:15 +0500

    Добавил возможность просмотра созданнй таблицы

---
+++ WowCaseApp/Forms/Table/TableInfoForm.cs
@@ -0,0 +1,50 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WowCaseApp.Model;
+
+namespace WowCaseApp
+{
+    public partial class TableInfoForm : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(NewTableForm));
+        private readonly MetaDataDBContainer metaDbContainer;
+        private readonly SqlConnection dbConnection;
+        private readonly Table currenTable;
+
+        public TableInfoForm(string realTableName, MetaDataDBContainer metaDataDbContainer, SqlConnection dbConnection)
+        {
+            InitializeComponent();
+            this.metaDbContainer = metaDataDbContainer;
+            this.dbConnection = dbConnection;
+            currenTable = metaDbContainer.TableSet.Where(t => t.RealName == realTableName).First();
+            Text = $"Просмотр таблицы {currenTable.Name}";
+
+            var allAttrs = currenTable.Attributes.ToList();
+            int allAttrsCount = allAttrs.Count;
+
+            if (allAttrsCount > 0)
+            {
+                dataGridView1.Rows.Add(allAttrsCount);
+
+                for (int i = 0; i < allAttrsCount; i++)
+                {
+                    var cells = dataGridView1.Rows[i].Cells;
+                    cells[0].Value = allAttrs[i].Name;
+                    cells[1].Value = (allAttrs[i].Type.StartsWith("t")) ? "Ссылка на таблицу" : allAttrs[i].Type;
+                    cells[2].Value = allAttrs[i].IsPKey;
+                    cells[3].Value = allAttrs[i].IsNullable;
+                    cells[4].Value = allAttrs[i].IsIndexed;
+                }
+            }
+        }
+    }
+}
